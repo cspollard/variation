4160b5c8e48f76ca2705d761f31f77ee34279787
Chris Pollard <cspollard@gmail.com>
2017-05-16 17:54:56 +0100

    some big changes.

--- src/Data/Variation.hs
+++ src/Data/Variation.hs
@@ -4,24 +4,28 @@
 {-# LANGUAGE DeriveTraversable          #-}
 {-# LANGUAGE FlexibleContexts           #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE TemplateHaskell            #-}
 {-# LANGUAGE TypeFamilies               #-}
 
 
 module Data.Variation
-  ( VariationT(..), Variations(..), nominal, variations, module X
+  ( VariationT(..), variation, variationT
+  , module X
   ) where
 
 import           Control.Applicative
 import           Control.DeepSeq
 import           Control.Lens             hiding ((<.>))
 import qualified Control.Monad            as M (join)
+import qualified Control.Monad.Fail       as MF
+import           Control.Monad.Morph
 import           Control.Monad.State      hiding (join)
 import           Control.Monad.Trans
 import           Control.Monad.Writer     hiding (join, (<>))
 import           Data.Functor.Apply
 import           Data.Functor.Bind
 import           Data.Functor.Classes
+import           Data.Functor.Product     (Product (Pair))
+import qualified Data.Functor.Product     as P
 import           Data.Semigroup
 import           Data.Serialize
 import           Data.SMonoid
@@ -29,36 +33,71 @@ import           Data.Variation.Instances as X
 import           GHC.Generics
 
 
-newtype VariationT f m a = VariationT { runVariationT :: m (Variations f a) }
+newtype VariationT f m a =
+  VariationT { runVariationT :: m (P.Product Identity f a) }
+  deriving (Generic, Functor, Foldable, Traversable)
 
-instance (Functor f, Functor m) => Functor (VariationT f m) where
-  fmap f = VariationT . (fmap.fmap) f . runVariationT
+type Variation f = VariationT f Identity
+
+runVariation :: VariationT f Identity a -> P.Product Identity f a
+runVariation = runIdentity . runVariationT
+
+fstP :: P.Product f g a -> f a
+fstP (Pair x _) = x
+
+sndP :: P.Product f g a -> g a
+sndP (Pair _ x) = x
+
+nominal :: Functor m => VariationT f m a -> m a
+nominal = fmap (runIdentity . fstP) . runVariationT
+
+variations :: Functor m => VariationT f m a -> m (f a)
+variations = fmap sndP . runVariationT
 
 instance (Apply f, SMonoid f, Applicative m) => Applicative (VariationT f m) where
-  pure = VariationT . pure . pure
-  VariationT f <*> VariationT x = VariationT $ liftA2 (<*>) f x
+  pure = VariationT . pure . flip Pair sempty . pure
 
+  VariationT f <*> VariationT x = VariationT $ g <$> f <*> x
+    where
+      g (Pair f1 f2) (Pair x1 x2) =
+        Pair
+          (f1 <.> x1)
+          ( (f2 <.> x2)
+            `sappend` fmap (runIdentity f1) x2
+            `sappend` fmap ($ runIdentity x1) f2
+          )
 
-instance
-  (Traversable f, Bind f, SMonoid f, Monad m)
-  => Monad (VariationT f m) where
-
+instance (Traversable f, Bind f, SMonoid f, Monad m) => Monad (VariationT f m) where
   return = pure
-  v >>= f = VariationT $ do
-    x <- runVariationT v
-    xs <- traverse (runVariationT . f) x
-    return $ M.join xs
+
+  VariationT x >>= f = VariationT $ do
+    -- fmap f <$> x :: m (Product Identity f (VariationT f m a))
+    (Pair (Identity vfmb) fvfmb) <- fmap f <$> x
+    (Pair nom fv) <- runVariationT vfmb
+    (Pair (Identity nv) ffb) <- runVariationT $ sequence fvfmb
+    return . Pair nom $ join ffb `sappend` nv `sappend` fv
+
 
 instance (Traversable f, Bind f, SMonoid f) => MonadTrans (VariationT f) where
-  lift = VariationT . fmap pure
+  lift = VariationT . fmap (flip Pair sempty . Identity)
 
 
 instance
   (MonadIO m, Traversable f, Bind f, SMonoid f)
   => MonadIO (VariationT f m) where
-
   liftIO = lift . liftIO
 
+
+instance (MF.MonadFail m, Traversable f, Bind f, SMonoid f)
+  => MF.MonadFail (VariationT f m) where
+  fail = lift . MF.fail
+
+
+instance MFunctor (VariationT f) where
+  hoist f = VariationT . f . runVariationT
+
+-- instance (SMonoid f, Bind f, Traversable f) => MMonad (VariationT f) where
+--   embed f (VariationT v) = undefined
 
 instance
   (Applicative m, Apply f, SMonoid f, Semigroup a)
@@ -71,52 +110,8 @@ instance
   mempty = pure mempty
   mappend = liftA2 mappend
 
+variation :: Applicative m => a -> f a -> VariationT f m a
+variation n vs = variationT (pure n) (pure vs)
 
-
-data Variations m a =
-  Variations
-    { _nominal    :: !a
-    , _variations :: !(m a)
-    } deriving (Generic, Show, Eq, Functor, Foldable, Traversable)
-
-
-instance (NFData a, NFData (m a)) => NFData (Variations m a) where
-
-instance (Serialize (m a), Serialize a) => Serialize (Variations m a)
-
-makeLenses ''Variations
-
-instance (Apply m, SMonoid m) => Applicative (Variations m) where
-  pure = flip Variations sempty
-  {-# INLINABLE pure #-}
-
-  Variations f fs <*> Variations x xs =
-    Variations (f x) ((fs <.> xs) `sappend` fmap f xs `sappend` fmap ($ x) fs)
-  {-# INLINABLE (<*>) #-}
-
-
-instance (Bind m, SMonoid m) => Monad (Variations m) where
-  return = pure
-  {-# INLINABLE return #-}
-
-  -- TODO
-  -- I'm not sure if mn or nm should be used here...
-  -- surely there's 100% overlap?
-  v >>= f =
-    let Variations (Variations n nm) mm = f <$> v
-        mn = view nominal <$> mm
-        mm' = view variations <$> mm
-    in Variations n (join mm' `sappend` nm `sappend` mn)
-  {-# INLINABLE (>>=) #-}
-
-
-instance (Semigroup a, SMonoid m, Apply m) => Semigroup (Variations m a) where
-  (<>) = liftA2 (<>)
-
-instance (Monoid a, SMonoid m, Apply m) => Monoid (Variations m a) where
-  mempty = Variations mempty sempty
-  mappend = liftA2 mappend
-
-instance Show1 m => Show1 (Variations m) where
-  liftShowsPrec sp sl d (Variations n m) =
-    showsBinaryWith sp (liftShowsPrec sp sl) "Variations" d n m
+variationT :: Applicative m => m a -> m (f a) -> VariationT f m a
+variationT n vs = VariationT $ Pair <$> (Identity <$> n) <*> vs
