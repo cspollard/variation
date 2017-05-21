199d3090c04a72da1564d02ed41c8c4f638ec31b
Chris Pollard <cspollard@gmail.com>
2017-05-21 16:09:05 +0100

    adding some convenience functions.

--- src/Data/Variation.hs
+++ src/Data/Variation.hs
@@ -9,27 +9,31 @@
 
 module Data.Variation
   ( VariationT(..), variation, variationT
+  , Variation, runVariation
+  , nominal, variations
   , module X
   ) where
 
 import           Control.Applicative
 import           Control.DeepSeq
-import           Control.Lens             hiding ((<.>))
-import qualified Control.Monad            as M (join)
-import qualified Control.Monad.Fail       as MF
+import           Control.Lens                hiding ((<.>))
+import qualified Control.Monad               as M (join)
+import qualified Control.Monad.Fail          as MF
 import           Control.Monad.Morph
-import           Control.Monad.State      hiding (join)
+import           Control.Monad.State         hiding (join)
 import           Control.Monad.Trans
-import           Control.Monad.Writer     hiding (join, (<>))
+import           Control.Monad.Trans.Compose
+import           Control.Monad.Trans.Maybe
+import           Control.Monad.Writer        hiding (join, (<>))
 import           Data.Functor.Apply
 import           Data.Functor.Bind
 import           Data.Functor.Classes
-import           Data.Functor.Product     (Product (Pair))
-import qualified Data.Functor.Product     as P
+import           Data.Functor.Product        (Product (Pair))
+import qualified Data.Functor.Product        as P
 import           Data.Semigroup
 import           Data.Serialize
 import           Data.SMonoid
-import           Data.Variation.Instances as X
+import           Data.Variation.Instances    as X
 import           GHC.Generics
 
 
@@ -48,11 +52,24 @@ fstP (Pair x _) = x
 sndP :: P.Product f g a -> g a
 sndP (Pair _ x) = x
 
-nominal :: Functor m => VariationT f m a -> m a
-nominal = fmap (runIdentity . fstP) . runVariationT
+getNominal :: Functor m => VariationT f m a -> m a
+getNominal = fmap (runIdentity . fstP) . runVariationT
 
-variations :: Functor m => VariationT f m a -> m (f a)
-variations = fmap sndP . runVariationT
+getVariations :: Functor m => VariationT f m a -> m (f a)
+getVariations = fmap sndP . runVariationT
+
+nominal :: Monad m => (a -> m a) -> VariationT f m a -> VariationT f m a
+nominal f (VariationT mp) = VariationT $ do
+  Pair (Identity x) y <- mp
+  x' <- f x
+  return $ Pair (Identity x') y
+
+variations :: Monad m => (f a -> m (f a)) -> VariationT f m a -> VariationT f m a
+variations f (VariationT mp) = VariationT $ do
+  Pair x y <- mp
+  y' <- f y
+  return $ Pair x y'
+
 
 instance (Apply f, SMonoid f, Applicative m) => Applicative (VariationT f m) where
   pure = VariationT . pure . flip Pair sempty . pure
@@ -115,3 +132,27 @@ variation n vs = variationT (pure n) (pure vs)
 
 variationT :: Applicative m => m a -> m (f a) -> VariationT f m a
 variationT n vs = VariationT $ Pair <$> (Identity <$> n) <*> vs
+
+
+-- class MComonad t where
+--   unembed :: Functor m => t m a -> m (t Identity a)
+--
+-- instance MComonad (VariationT f) where
+--   unembed (VariationT x) = VariationT . Identity <$> x
+--
+-- instance MComonad (WriterT w) where
+--   unembed (WriterT x) = WriterT . Identity <$> x
+--
+-- instance MComonad MaybeT where
+--   unembed (MaybeT x) = MaybeT . Identity <$> x
+--
+--
+-- class Asdf f where
+--   asdf :: (MonadTrans g, Monad m, Monad (g m)) => g m (f m a) -> f (g m) a
+--
+-- instance Asdf MaybeT where
+--   asdf x = MaybeT $ M.join $ lift . runMaybeT <$> x
+--
+--
+-- instance (Functor (g Identity), Asdf f, MonadTrans g, Monad (g Identity), MComonad f, MComonad g) => MComonad (ComposeT f g) where
+--   unembed = fmap (ComposeT . asdf) . unembed . unembed . getComposeT
